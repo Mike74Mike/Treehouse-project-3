9410fd6b34f4cdb487a5e482a0729aff4e0b84c2
Patricia Tigue <patriciatigue@patricias-imac.attlocal.net>
2021-03-26 00:04:18 -0500

    This is my tree house project 3 i'm submitting this for review

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/**
+*@file - This adds design and validation to the form fields
+*@author Mike Mike
+*/
+
+/**
+*@const - selecting multiple elements in a page
+*@type {Element}
+*/
 const form =document.querySelector('form');
 const name = document.querySelector('#name');
 const email = document.querySelector('#email');
@@ -9,12 +18,22 @@ const zipCode = document.querySelector('#zip');
 const cvv = document.querySelector('#cvv');
 
 
-
+/**
+*@function focusName
+*@description- focus on the Name input
+*/
 const focusName = () => {
   document.querySelector('#name').focus();
 }
 focusName();
+//callback focusName
 
+
+/**
+*@function otherJob
+*@description - when you select other under the job role label will display
+*               a text input
+*/
 function otherJob () {
   const otherRole= document.querySelector('#other-job-role');
   const title= document.querySelector('#title');
@@ -29,7 +48,13 @@ function otherJob () {
   })
 }
 otherJob();
+// call back otherJobe
 
+/**
+*@function colorScheme
+*@description - When a certain design theme is selected it the color dropdown
+*                below will display congruous color scheme
+*/
 function colorScheme(){
   const color = document.querySelector('#color');
 
@@ -56,7 +81,13 @@ function colorScheme(){
   })
 }
 colorScheme();
+//calling colorScheme
 
+/**
+*@function totalAmount
+*@description - When a priced checkbox is checked the price will be added to
+*               to the total <p>
+*/
 function totalAmount() {
   const activities = document.querySelector('#activities');
   const total =document.querySelector('#activities-cost');
@@ -74,16 +105,24 @@ function totalAmount() {
   })
 }
 totalAmount();
+//calling totalAmount
 
+/**
+*@function paymentInfo
+*@description - In the payment info drop down when you select a certain pay option
+*               it will either show/hide relevant information
+*/
 function paymentInfo() {
   const select = document.querySelectorAll('#payment')[0];
   const selectSecond = select.children[1];
   const creditCard = document.querySelector('#credit-card');
   const paypal = document.querySelector('#paypal');
   const bitcoin = document.querySelector('#bitcoin');
+  const creditCardBox = document.querySelector('.credit-card-box');
 
   paypal.hidden = true;
   bitcoin.hidden = true;
+
 
   selectSecond.selected =true;
 
@@ -97,13 +136,22 @@ function paymentInfo() {
     } else{
       bitcoin.hidden = true;
       paypal.hidden =true;
+    } if(bitcoin.id === e.target.value || paypal.id === e.target.value ){
+      creditCardBox.style.display = 'none';
+    } else{
+      creditCardBox.style.display ='flex'
     }
   })
 }
 paymentInfo();
+//calling paymentInfo
 
+/**
+*@function formValidator
+*@description - this function stops the form from being submitted when required
+*               information and displays error text when a form isn't filled out
+*/
 function formValidator(){
-
 
   function nameValidator() {
     const nameValue = name.value;
@@ -145,23 +193,70 @@ function formValidator(){
       e.preventDefault()
     }
 
+    if(!nameValidator()){
+      name.classList.add('not-valid');
+      name.classList.remove('valid');
+      name.parentNode.lastElementChild.style.display = 'block';
+    } else if(nameValidator()){
+      name.classList.add('valid');
+      name.classList.remove('not-valid');
+      name.parentNode.lastElementChild.style.display = 'none';
+    } if(!emailValidator()){
+      email.classList.add('not-valid');
+      email.classList.remove('valid');
+      email.parentNode.lastElementChild.style.display = 'block';
+    } else if(emailValidator()){
+      email.classList.add('valid');
+      email.classList.remove('not-valid');
+      email.parentNode.lastElementChild.style.display = 'none';
+    }  if(!creditCardValidator()) {
+      creditCard.classList.add('not-valid');
+      creditCard.classList.remove('valid');
+      creditCard.parentNode.lastElementChild.style.display ='block'
+    } else if(creditCardValidator()) {
+      creditCard.classList.add('valid');
+      creditCard.classList.remove('not-valid');
+      creditCard.parentNode.lastElementChild.style.display = 'none';
+    }  if(!zipCodeValidator()) {
+      zipCode.classList.add('not-valid');
+      zipCode.classList.remove('valid');
+      zipCode.parentNode.lastElementChild.style.display = 'block';
+    } else if(zipCodeValidator()) {
+      zipCode.classList.add('valid');
+      zipCode.classList.remove('not-valid');
+      zipCode.parentNode.lastElementChild.style.display = 'none'
+    }  if(!cvvValidator()) {
+      cvv.classList.add('not-valid');
+      cvv.classList.remove('valid');
+      cvv.parentNode.lastElementChild.style.display = 'block'
+    } else if(cvvValidator()){
+      cvv.classList.add('valid');
+      cvv.classList.remove('not-valid');
+      cvv.parentNode.lastElementChild.style.display = 'none'
+    }
   })
-
-
-
 }
 formValidator()
+//calling formValidator
 
-function formValidation(){
+/**
+*@function checkboxFocus
+*@description - when a checkbox is clicked the parent of the checkbox will be given
+*               the 'focus' class name
+*/
+function checkboxFocus(){
   const checkbox = document.querySelectorAll('input[type="checkbox"]');
-  console.log(checkbox.parentElement)
+
   for(let i = 0; i < checkboxes.length; i++){
-    checkbox.addEventListener('focus', e =>{
-      e.parentNode.classList.add('focus')
-    })
-    checkbox.addEventListener('blur', e =>{
-      e.parentNode.classList.remove('focus')
+
+    checkbox[i].addEventListener('click', () =>{
+      if(checkbox[i].checked){
+      checkbox[i].parentNode.classList.add('focus')
+    } else{
+      checkbox[i].parentNode.classList.remove('focus')
+    }
     })
   }
 }
-formValidation()
+checkboxFocus()
+//calling checkboxFocus
